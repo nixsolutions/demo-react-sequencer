f31c12edf2da0ba83de31eef28251d582965b5b2
jsruvi <jsruvi@gmail.com>
2016-11-29 16:29:12 +0200

    started ToggleButton

---
+++ public/components/common/toggleButton/ToggleButton.js
@@ -0,0 +1,11 @@
+import React, {Component, PropTypes} from 'react';
+
+class ToggleButton extends Component {
+    render() {
+        
+    }
+}
+
+ToggleButton.propTypes = {};
+
+export default ToggleButton;
