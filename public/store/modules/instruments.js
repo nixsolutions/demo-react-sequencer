d75a9271701d9ca25549d4eec5d3298ecc407b00
jsruvi <jsruvi@gmail.com>
2016-11-30 15:33:45 +0200

    renamed matrix reducer to instruments

--- public/store/modules/instruments.js
+++ public/store/modules/instruments.js
@@ -1,26 +1,26 @@
-export const UPDATE_MATRIX = 'UPDATE_MATRIX';
+export const UPDATE_INSTRUMENTS = 'UPDATE_INSTRUMENTS';
 
 const INIT = [
     {
-        active: 1,
+        active: true,
         path: './samples/hip-hop_kick.wav',
         notes: [0, 0, undefined, undefined,  undefined, 0, undefined, undefined]
     },
     {
-        active: 1,
+        active: true,
         path: './samples/hip-hop_snare.wav',
         notes: [undefined, undefined, undefined, 0, undefined, undefined, undefined, 0]
     },
     {
-        active: 1,
+        active: true,
         path: './samples/techno_hi-hat.wav',
         notes: [0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 ];
 
-export default function matrixReducer(state = INIT, action){
+export default function instrumentsReducer(state = INIT, action){
     switch(action.type){
-        case UPDATE_MATRIX:
+        case UPDATE_INSTRUMENTS:
             return action.payload;
         default:
             return state;
