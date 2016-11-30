97ead457ede3aabd13f568db33b916882d900a07
jsruvi <jsruvi@gmail.com>
2016-11-30 15:11:09 +0200

    matrix fake data

--- public/store/modules/matrix.js
+++ public/store/modules/matrix.js
@@ -1,10 +1,21 @@
 export const UPDATE_MATRIX = 'UPDATE_MATRIX';
 
 const INIT = [
-    {active: 1},
-    {active: 1},
-    {active: 2},
-    {active: 3},
+    {
+        active: 1,
+        path: './samples/hip-hop_kick.wav',
+        notes: [0, 0, undefined, undefined,  undefined, 0, undefined, undefined]
+    },
+    {
+        active: 1,
+        path: './samples/hip-hop_snare.wav',
+        notes: [undefined, undefined, undefined, 0, undefined, undefined, undefined, 0]
+    },
+    {
+        active: 1,
+        path: './samples/techno_hi-hat.wav',
+        notes: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
 ];
 
 export default function matrixReducer(state = INIT, action){
