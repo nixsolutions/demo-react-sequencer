b2aa2a2a0c36f56fc839cf3179dae01c1337d0e4
jsruvi <jsruvi@gmail.com>
2016-11-30 11:38:46 +0200

    added sequenceEditor component

---
+++ public/components/blocks/sequenceEditor/SequenceEditor.js
@@ -0,0 +1,25 @@
+import CSSModules from 'react-css-modules';
+import styles from './styles.less';
+import React, {Component, PropTypes} from 'react';
+import Step from './step/Step';
+
+class SequenceEditor extends Component {
+    render() {
+        let {steps} = this.props; 
+        let items = this.createSteps(steps);
+        return <ul styleName="sequence-editor">{items}</ul>
+    }
+
+    createSteps(steps){
+        return steps.map((step, i) => {
+            let isEven = Math.floor(i / 4) % 2 !== 0;
+            return <li><Step active={step.active} isEven={isEven}/></li>;
+        })
+    }
+}
+
+SequenceEditor.propTypes = {
+    children: PropTypes.node
+};
+
+export default CSSModules(SequenceEditor, styles, {allowMultiple: true});
