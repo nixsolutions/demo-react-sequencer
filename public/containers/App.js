18fad62f9e509c6fa3b94c3ab4e25141ec3d79e8
Aleksandr Moiseev <moiseev@nixsolutions.com>
2016-11-30 16:13:33 +0200

    all buttons

--- public/containers/App.js
+++ public/containers/App.js
@@ -1,12 +1,17 @@
 import 'shared/reset.less';
+import 'shared/layouts.less';
 import React, { Component } from 'react';
 import PlayButton from 'components/common/buttons/playButton/PlayButton';
+import PauseButton from 'components/common/buttons/pauseButton/PauseButton';
+import StopButton from 'components/common/buttons/stopButton/StopButton';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <PlayButton active={true}></PlayButton>
+         <PlayButton active={true}></PlayButton>
+         <PauseButton active={true}></PauseButton>
+         <StopButton active={true}></StopButton>
       </div>
     );
   }
