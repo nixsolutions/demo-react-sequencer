eb5fd065d77701ef6b04b1aeac624e8219ad4e74
jsruvi <jsruvi@gmail.com>
2016-11-30 18:42:37 +0200

    bpmEditor component

---
+++ public/components/blocks/bpmEditor/BpmEditor.js
@@ -0,0 +1,29 @@
+import CSSModules from 'react-css-modules';
+import styles from './styles.less';
+import React, {Component, PropTypes} from 'react';
+
+class BpmEditor extends Component {
+    render() {
+        return (
+            <div styleName="bpm-editor">
+                <span styleName="title">BPM</span>
+                <input type="text" 
+                        onChange={this.onChange.bind(this)} 
+                        value={this.props.value}
+                        ref="bpmInput"/>
+            </div>
+        );
+    }
+
+    onChange(){
+        let value = parseInt(this.refs.bpmInput.value) || 1;
+        this.props.onChange(value);
+    }
+}
+
+BpmEditor.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func
+};
+
+export default CSSModules(BpmEditor, styles, {allowMultiple: true});
