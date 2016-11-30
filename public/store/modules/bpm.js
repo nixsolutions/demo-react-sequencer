f9ac22eae0ba780e10db52f61d460643c15cc177
jsruvi <jsruvi@gmail.com>
2016-11-30 12:46:33 +0200

    updated reducers

--- public/store/modules/bpm.js
+++ public/store/modules/bpm.js
@@ -2,7 +2,7 @@ export const UPDATE_BPM = 'UPDATE_BPM';
 
 const INIT = 120;
 
-export function bpm(state = INIT, action){
+export default function bpmReducer(state = INIT, action){
     switch(action.type){
         case UPDATE_BPM:
             return action.payload;
