fd84fb0b0ab3f29aca346f9d7d35f6b41c3e5149
jsruvi <jsruvi@gmail.com>
2016-12-02 18:24:18 +0200

    analyser reducer

--- public/store/modules/index.js
+++ public/store/modules/index.js
@@ -2,10 +2,12 @@ import bpm from './bpm';
 import instruments from './instruments';
 import play from './play';
 import playedStep from './playedStep';
+import analyser from './analyser';
 
 export default {
     bpm,
     instruments,
     play,
-    playedStep
+    playedStep,
+    analyser
 };
