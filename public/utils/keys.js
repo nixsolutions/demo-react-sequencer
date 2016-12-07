95df235021679682394ecb2da69a374d874548c9
jsruvi <jsruvi@gmail.com>
2016-12-07 18:49:10 +0200

    keys utils

---
+++ public/utils/keys.js
@@ -0,0 +1,5 @@
+export const FIRST_OCTAVE_KEYS = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187];
+export const SECOND_OCTAVE_KEYS = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221];
+export const THIRD_OCTAVE_KEYS = [65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220];
+
+export const PIANO_KEYS = [].concat(FIRST_OCTAVE_KEYS, SECOND_OCTAVE_KEYS, THIRD_OCTAVE_KEYS);
