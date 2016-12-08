cb84594edd861d6da722313d828c00bbbc53c90e
jsruvi <jsruvi@gmail.com>
2016-12-08 17:53:32 +0200

    updated samples

--- public/store/modules/samples.js
+++ public/store/modules/samples.js
@@ -12,6 +12,26 @@ const INIT = [
     {
         name: 'hi-hat',
         path: './samples/techno_hi-hat.wav'
+    },
+    {
+        name: 'afro latin bell',
+        path: './samples/afro-latin_bell.wav'
+    },
+    {
+        name: 'afro latin bongo',
+        path: './samples/afro-latin_bongo_high.wav'
+    },
+    {
+        name: 'afro latin conga low',
+        path: './samples/afro-latin_conga_low.wav'
+    },
+    {
+        name: 'bells',
+        path: './samples/bells.wav'
+    },
+    {
+        name: 'piano',
+        path: './samples/piano.wav'
     }
 ];
 
