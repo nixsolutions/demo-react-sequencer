9365f0fba0b73138ce80f861e3865a93a32497d7
jsruvi <jsruvi@gmail.com>
2016-12-08 12:52:44 +0200

    added volume control to piano

--- public/containers/accompaniment/Accompaniment.js
+++ public/containers/accompaniment/Accompaniment.js
@@ -1,6 +1,10 @@
+import CSSModules from 'react-css-modules';
+import styles from './styles.less';
 import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux';
 import Piano from 'components/blocks/piano/Piano';
+import Controller from 'components/common/controller/Controller';
+import VerticalControls, {VerticalItem} from 'components/common/verticalControls/VerticalControls';
 import PianoManager from './PianoManager';
 import Tone from 'tone';
 import {addPlayedNote, removePlayedNote} from 'modules/playedNotes';
@@ -9,10 +13,21 @@ import {bindToKey} from 'modules/bindings';
 class Accompaniment extends Component {
     render(){ 
         return <div>
-            <Piano onKeyDown={this.props.addPlayedNote}  
-                    onKeyUp={this.props.removePlayedNote}
-                    bindToKey={this.props.bindToKey}/>
-            <PianoManager/>
+            <div styleName="section-holder">
+                <div styleName="controls-holder">
+                    <VerticalControls>
+                        <VerticalItem label="volume"> 
+                            <Controller size="30"/>
+                        </VerticalItem>
+                    </VerticalControls>
+                </div>
+                <div styleName="instrument-holder">
+                    <Piano onKeyDown={this.props.addPlayedNote}  
+                        onKeyUp={this.props.removePlayedNote}
+                        bindToKey={this.props.bindToKey}/>
+                    <PianoManager/>
+                </div>
+            </div>
         </div> 
     }
 }
@@ -27,7 +42,7 @@ export default connect(mapStateToProps, {
     addPlayedNote,
     removePlayedNote,
     bindToKey
-})(Accompaniment);
+})(CSSModules(Accompaniment, styles));
 
 function mapStateToProps(state){
     return {
