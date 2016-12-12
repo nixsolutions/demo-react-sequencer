26db903ce37afdac4d69435f2c9c09b49f7c1fa1
jsruvi <jsruvi@gmail.com>
2016-12-12 13:51:28 +0200

    changed keyCodes for octaves

--- public/utils/keys.js
+++ public/utils/keys.js
@@ -1,6 +1,6 @@
-export const FIRST_OCTAVE_KEYS = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187];
-export const SECOND_OCTAVE_KEYS = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221];
-export const THIRD_OCTAVE_KEYS = [65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 220];
+export const FIRST_OCTAVE_KEYS = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
+export const SECOND_OCTAVE_KEYS = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71];
+export const THIRD_OCTAVE_KEYS = [72, 73, 74, 75, 76, 77, 78, 79];
 
 export const PIANO_KEYS = [].concat(FIRST_OCTAVE_KEYS, SECOND_OCTAVE_KEYS, THIRD_OCTAVE_KEYS);
 export const SPACE_KEY = 32;
