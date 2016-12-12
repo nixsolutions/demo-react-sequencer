cf4b662653cc46edee4bd81b9c632f92a7205ae4
jsruvi <jsruvi@gmail.com>
2016-12-12 11:08:22 +0200

    reworked tabs

--- public/components/common/tabs/Tabs.js
+++ public/components/common/tabs/Tabs.js
@@ -1,28 +1,45 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
 import React, { Component, PropTypes } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
-Tabs.setUseDefaultStyles(false);
 
 class TabsComponent extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {selectedIndex: 0};
+    }
+
     render() {
-        let tabsList = React.Children.map(this.props.children, child => {
-            return <Tab>{child.props.label}</Tab>;
+        let tabsList = React.Children.map(this.props.children, (child, i) => {
+            let activeClass = (this.state.selectedIndex === i) ? 'active' : '';
+
+            return <li styleName={activeClass} 
+                        onClick={this.onSelect.bind(this, i)}>
+                        {child.props.label}
+                    </li>;
         });
 
-        let content = React.Children.map(this.props.children, child => {
-            return <TabPanel styleName="tab-content">{child}</TabPanel>;
+        let content = React.Children.map(this.props.children, (child, i) => {
+            let activeClass = (this.state.selectedIndex === i) ? 'active' : '';
+            let tabClass = ['tab', activeClass].join(' ');
+
+            return <div styleName={tabClass}>{child}</div>;
         });
-        return (<Tabs onSelect={this.handleSelect} selectedIndex={0}>
-                    <TabList styleName="tab-controls">{tabsList}</TabList>
-                    {content}
-                </Tabs>);
+
+        return (<div  styleName="tabs" selectedIndex={0}>
+                    <ul styleName="tab-controls">{tabsList}</ul>
+                    <div styleName="tab-content">{content}</div>
+                </div>);
+    }
+
+    onSelect(tabIndex){
+        this.setState({selectedIndex: tabIndex});
     }
 }
 
 TabsComponent.propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    selectedIndex: PropTypes.number,
 };
 
 export default CSSModules(TabsComponent, styles, { allowMultiple: true });
