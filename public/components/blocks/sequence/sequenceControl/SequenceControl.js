c82469286e3833ed4ee8468bad6695facccf8f34
jsruvi <jsruvi@gmail.com>
2016-12-14 12:02:14 +0200

    showing instrument effects popup

--- public/components/blocks/sequence/sequenceControl/SequenceControl.js
+++ public/components/blocks/sequence/sequenceControl/SequenceControl.js
@@ -1,8 +1,9 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import Controller from 'components/common/controller/Controller';
 import Popup from 'components/common/popup/Popup';
+import Effects from 'components/blocks/effects/Effects';
 
 class SequenceControl extends Component {
     render() {
@@ -11,26 +12,26 @@ class SequenceControl extends Component {
         let muteText = instrument.active ? 'disable instrument' : 'enable instrument';
 
         return (
-            <div styleName="sequence-control">
-                <div styleName={muteClass} 
-                    title={muteText} 
+            <div styleName="sequence-control" ref="sequence-control">
+                <div styleName={muteClass}
+                    title={muteText}
                     onClick={this.props.toggleInstrument.bind(this, instrument)}></div>
                 <div styleName="instrument-block">
                     <div styleName="name" title={instrument.name}>{instrument.name}</div>
                     <div styleName="controller">
-                        <Controller size="25" value={instrument.volume} onChange={this.updateInstrumentVolume.bind(this, instrument)}/>
+                        <Controller size="25" value={instrument.volume} onChange={this.updateInstrumentVolume.bind(this, instrument)} />
                     </div>
                     <div styleName="controller">
                         <span onClick={this.onFxClick.bind(this, instrument)} styleName="fx-button">FX</span>
                     </div>
-                    <div styleName="remove" 
+                    <div styleName="remove"
                         onClick={this.onRemoveClick.bind(this, instrument)}>X</div>
                 </div>
             </div>
         );
     }
 
-    onRemoveClick(instrument){
+    onRemoveClick(instrument) {
         let {removeInstrument} = this.props;
 
         Popup.show({
@@ -39,14 +40,14 @@ class SequenceControl extends Component {
             buttons: [
                 {
                     title: 'Yes',
-                    click: function() {
+                    click: function () {
                         removeInstrument(instrument);
                         this.onClose();
                     }
                 },
                 {
                     title: 'No',
-                    click: function(){
+                    click: function () {
                         this.onClose();
                     }
                 }
@@ -54,16 +55,50 @@ class SequenceControl extends Component {
         });
     }
 
-    onFxClick(instrument){
-        let {removeInstrument} = this.props;
+    onFxClick(instrument) {
+        let {removeInstrument, styles} = this.props;
+        let content = CSSModules(<div styleName="effects-block">
+            <Effects effects={this.props.effects}
+                    remove={this.props.removeInstrumentEffect}
+                    toggleMute={this.props.toggleMuteInstrumentEffect}
+                    changeWet={this.props.changeWetInstrumentEffect}
+                    changeSetting={this.props.changeSettingInstrumentEffect}/>
+        </div>, styles);
+
+        let sequenceControlElement = this.refs['sequence-control'];
+        let coords = this.getCoords(sequenceControlElement);
 
         Popup.show({
             title: `${instrument.name}'s effects`,
-            content: <a>sdfsdfsd</a>
+            position: { x: coords.right, y: coords.top },
+            content
         });
     }
 
-    updateInstrumentVolume(instrument, volume){
+    getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+
+        var body = document.body;
+        var docEl = document.documentElement;
+
+        var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+        var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+        var clientTop = docEl.clientTop || body.clientTop || 0;
+        var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+        var top = box.top + scrollTop - clientTop;
+        var left = box.left + scrollLeft - clientLeft;
+
+        return {
+            top: top,
+            left: left,
+            right: left + elem.clientWidth,
+            bottom: top + elem.clientHeigth,
+        };
+    }
+
+    updateInstrumentVolume(instrument, volume) {
         this.props.updateInstrumentVolume(instrument, volume);
     }
 }
@@ -78,6 +113,12 @@ SequenceControl.propTypes = {
     toggleInstrument: PropTypes.func,
     removeInstrument: PropTypes.func,
     updateInstrumentVolume: PropTypes.func,
+
+    addInstrumentEffect: PropTypes.func,
+    removeInstrumentEffect: PropTypes.func,
+    toggleMuteInstrumentEffect: PropTypes.func,
+    changeWetInstrumentEffect: PropTypes.func,
+    changeSettingInstrumentEffect: PropTypes.func,
 };
 
-export default CSSModules(SequenceControl, styles, {allowMultiple: true});
+export default CSSModules(SequenceControl, styles, { allowMultiple: true });
