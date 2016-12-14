8a6bb9e02d8eb655240f1e2efcc24457862ede54
jsruvi <jsruvi@gmail.com>
2016-12-14 17:03:30 +0200

    default styles for modal

--- public/components/common/modal/Modal.js
+++ public/components/common/modal/Modal.js
@@ -1,6 +1,6 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import Modal from 'react-modal';
 
 class ModalComponent extends Component {
@@ -11,24 +11,43 @@ class ModalComponent extends Component {
 
         let positionClass = options.position ? 'positioned' : '';
 
-        return this.create(Object.assign({}, options, {className: positionClass}));
+        return this.create(Object.assign({}, options, { className: positionClass }));
     }
 
     render() {
         let head = <div styleName="head">
-                    <span styleName="title">{this.props.title}</span>
-                    <span  styleName="close" onClick={this.onClose}>x</span> 
-                </div>;
+            <span styleName="title">{this.props.title}</span>
+            <span styleName="close" onClick={this.props.onRequestClose}>x</span>
+        </div>;
 
         let buttons = (this.props.buttons || []).map((button, i) => {
-            return <button key={i} 
-                    onClick={button.click.bind(this)} 
-                    styleName="button">{button.title}</button>;
+            return <button key={i}
+                onClick={button.click.bind(this)}
+                styleName="button">{button.title}</button>;
         });
 
         let buttonsBlock = buttons.length ? <div styleName="buttons">{buttons}</div> : '';
 
-        return <Modal {...this.props}>
+        const customStyles = {
+            overlay: Object.assign({
+                background: 'rgba(0, 0, 0, 0.5)',
+                zIndex: 999
+            }, this.props.style && this.props.style.overlay),
+            content: Object.assign({
+                top: '50%',
+                left: '50%',
+                right: 'auto',
+                bottom: 'auto',
+                marginRight: '-50%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: 999,
+                background: 'none',
+                border: 0,
+                padding: 0
+            }, this.props.style && this.props.style.content)
+        };
+
+        return <Modal {...this.props} style={customStyles}>
             <div styleName="modal">
                 {head}
                 <div styleName='content'>{this.props.children}</div>
@@ -44,4 +63,4 @@ ModalComponent.propTypes = {
     children: PropTypes.node,
 };
 
-export default CSSModules(ModalComponent, styles, {allowMultiple: true});
+export default CSSModules(ModalComponent, styles, { allowMultiple: true });
