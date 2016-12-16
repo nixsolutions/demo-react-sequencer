f3665c5dfea088132823ff3574e37840d27b0f64
jsruvi <jsruvi@gmail.com>
2016-12-16 18:41:17 +0200

    started reworking analyser

--- public/components/blocks/analyser/Analyser.js
+++ public/components/blocks/analyser/Analyser.js
@@ -4,17 +4,24 @@ import React, {Component, PropTypes} from 'react';
 
 class Analyser extends Component {
     static defaultProps = {
-        width: 100,
-        height: 30
+        width: 200,
+        height: 130,
+        segmentsInStack: 40,
     };
 
-    constructor(props, state){
-        super(props, state);
-    }
+    componentWillReceiveProps(props){
+        if(props.analyser && !this.analyserContext){
+            this.padding = 1;
+            this.startChanalValue = 150;
+            this.colorIncreaseStep = 10;
+            this.segmentWeight = 255 / this.props.segmentsInStack;
+            this.segmentWidth = (this.props.width / props.analyser.size) - this.padding;
+            this.segmentHeight = (this.props.height / this.props.segmentsInStack) - this.padding;
 
-    componentDidMount(){
-        this.analyserContext = this.refs.canvas.getContext("2d");
-        this.loop();
+            this.analyserContext = this.refs.canvas.getContext("2d");
+
+            this.loop();
+        }
     }
 
     render() {
@@ -30,21 +37,32 @@ class Analyser extends Component {
                 </div>;
     }
 
-    drawAnalyser(values){
-        let canvasWidth = this.props.width;
-        let canvasHeight = this.props.height;
+    draw(values){
+        this.clear();
+        this.drawSegments(values);
+    }
 
-        this.analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        let barWidth = canvasWidth / this.props.analyser.size;
+    clear(){
+        this.analyserContext.clearRect(0, 0, this.props.width, this.props.height);
+    }
 
+    drawSegments(values){
         values.forEach((value, i) => {
-            let saturation = value / 255;
-            let x = barWidth * i;
-            let y = saturation * canvasHeight;
+            let activeSegmentsAmount = value / this.segmentWeight;
 
-            this.analyserContext.fillStyle = `rgba(0, 0, 0, ${saturation})`;
-            this.analyserContext.fillRect(x, canvasHeight - y, barWidth, canvasHeight);
+            for(let j = 0; activeSegmentsAmount > j; j++){
+                this.drawSegment(i, j);
+            }
         });
+    }
+
+    drawSegment(colIndex, indexInStack){
+        let chanalValue = this.startChanalValue + (this.colorIncreaseStep * indexInStack);
+        let x = (this.segmentWidth * colIndex) + (colIndex * this.padding);
+        let y = this.props.height - ((indexInStack - 1) * this.segmentHeight) - (indexInStack * this.padding);
+
+        this.analyserContext.fillStyle = `rgb(243, ${chanalValue}, 55)`;
+        this.analyserContext.fillRect(x, y, this.segmentWidth, this.segmentHeight);
     }
 
     loop(){
@@ -52,13 +70,14 @@ class Analyser extends Component {
 
         if(this.props.analyser){
             var analyserValues = this.props.analyser.analyse();
-            this.drawAnalyser(analyserValues);
+            this.draw(analyserValues);
         }
     }
 }
 
 Analyser.propTypes = {
     analyser: PropTypes.object,
+    segmentsInStack: PropTypes.number,
     width: PropTypes.number,
     height: PropTypes.number
 };
