48db78f251af5adc5d4970eacf5d50352a5c9bc6
jsruvi <jsruvi@gmail.com>
2016-12-16 12:42:03 +0200

    horizontal indicator

---
+++ public/components/blocks/volumeController/indicator/Indicator.js
@@ -0,0 +1,46 @@
+import CSSModules from 'react-css-modules';
+import styles from './styles.less';
+import React, {Component, PropTypes} from 'react';
+
+class Indicator extends Component {
+    static defaultProps = {
+        value: 0,
+        amount: 10,
+        initItemHeight: 5,
+        increaseValue: 3
+    }
+
+    render() {
+        let items = [];
+
+        for(let i = 0; items.length < this.props.amount; i++){
+            let className = this.isActiveItem(i) ? 'active' : '';
+            items.push(<li key={i} 
+                        styleName={className}
+                        style={{height: this.getItemHeight(i)}}></li>);
+        };
+
+        return <ul styleName="indicator">{items}</ul>;
+    }
+
+    isActiveItem(itemIndex){
+        let ITEM_WEIGHT = 100 / this.props.amount;
+        let itemValue = itemIndex * ITEM_WEIGHT;
+
+        return this.props.value > itemValue;
+    }
+
+    getItemHeight(itemIndex){
+        let {initItemHeight, increaseValue} = this.props;
+        return initItemHeight + (increaseValue * itemIndex);
+    }
+}
+
+Indicator.propTypes = {
+    value: PropTypes.number,
+    amount: PropTypes.number,
+    initItemHeight: PropTypes.number,
+    increaseValue: PropTypes.number,
+};
+
+export default CSSModules(Indicator, styles, {allowMultiple: true});
