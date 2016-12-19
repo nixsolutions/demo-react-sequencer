fc1d8f3e9a81cc5c7b9b3a2776026848e53d8231
jsruvi <jsruvi@gmail.com>
2016-12-19 13:50:18 +0200

    completed analyser

--- public/components/blocks/analyser/Analyser.js
+++ public/components/blocks/analyser/Analyser.js
@@ -1,109 +1,116 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Analyser extends Component {
     static defaultProps = {
-        width: 300,
-        height: 130,
         segmentsInStack: 20,
-        gridColor: '#f26522',
+        gridColor: 'rgba(241, 101, 34, 0.2)',
     };
 
-    componentWillReceiveProps(props){
-        if(props.analyser && !this.analyserContext){
-            this.padding = 2;
+    componentWillReceiveProps(props) {
+        if (props.analyser && !this.analyserContext) {
+            this.analyserContext = this.refs.segments.getContext("2d");
+            this.padding = 1;
             this.startChanalValue = 150;
             this.colorIncreaseStep = 10;
+            this.segmentWidth =7;
+            this.segmentHeight = 2;
             this.segmentWeight = 255 / this.props.segmentsInStack;
-            this.segmentWidth = (this.props.width / props.analyser.size) - this.padding;
-            this.segmentHeight = (this.props.height / this.props.segmentsInStack) - this.padding;
-
-            this.analyserContext = this.refs.canvas.getContext("2d");
+            this.segmentFullWidth = this.segmentWidth + this.padding;
+            this.segmentFullHeight = this.segmentHeight + this.padding;
+            this.analyserSize = props.analyser.size;
+            this.width = this.segmentFullWidth * this.analyserSize - this.padding;
+            this.height = this.segmentFullHeight * this.props.segmentsInStack - this.padding;
 
             this.loop();
+            setTimeout(() => this.drawGrid())
         }
     }
 
     render() {
         let style = {
-            width: this.props.width,
-            height: this.props.height
+            width: this.width,
+            height: this.height
         };
 
         return <div styleName="analyser" style={style}>
-                    <canvas width={this.props.width}
-                            height={this.props.height}
-                            ref="canvas"></canvas>
+                    <canvas width={this.width}
+                        height={this.height}
+                        ref="grid"></canvas>
+                    <canvas width={this.width}
+                        height={this.height}
+                        ref="segments"></canvas>
                 </div>;
     }
 
-    draw(values){
+    draw(values) {
         this.clear();
         this.drawSegments(values);
-        this.drawGrid();
     }
 
-    clear(){
-        this.analyserContext.clearRect(0, 0, this.props.width, this.props.height);
+    clear() {
+        this.analyserContext.clearRect(0, 0, this.width, this.height);
     }
 
-    drawSegments(values){
+    drawSegments(values) {
         values.forEach((value, i) => {
             let activeSegmentsAmount = value / this.segmentWeight;
 
-            for(let j = 0; activeSegmentsAmount > j; j++){
+            for (let j = 0; activeSegmentsAmount > j; j++) {
                 this.drawSegment(i, j);
             }
         });
     }
 
-    drawSegment(colIndex, indexInStack){
+    drawSegment(colIndex, indexInStack) {
         let chanalValue = this.startChanalValue + (this.colorIncreaseStep * indexInStack);
-        let x = (this.segmentWidth * colIndex) + (colIndex * this.padding);
-        let y = this.props.height - ((indexInStack - 1) * this.segmentHeight) - (indexInStack * this.padding);
+        let x = this.segmentFullWidth * colIndex;
+        let y = this.height - ((indexInStack) * this.segmentFullHeight) - this.segmentHeight;
 
         this.analyserContext.fillStyle = `rgb(243, ${chanalValue}, 55)`;
         this.analyserContext.fillRect(x, y, this.segmentWidth, this.segmentHeight);
     }
 
-    drawGrid(){
+    drawGrid() {
+        this.gridContext = this.refs.grid.getContext("2d");
+
         this.drawVerticalGridLines();
         this.drawHorizontalGridLines();
     }
 
-    drawVerticalGridLines(){
-        for(let i = 0; this.props.analyser.size > i; i++){
-            let segmentCellXFrom = (this.segmentWidth + this.padding) * i + 0.5;
+    drawVerticalGridLines() {
+        for (let i = 0; this.analyserSize > i; i++) {
+            let segmentCellXFrom = (this.segmentFullWidth) * i + 0.5;
             let segmentCellXTo = segmentCellXFrom + this.segmentWidth;
 
-            this.drawGridLine(segmentCellXFrom, 0, segmentCellXFrom, this.props.height);
-            this.drawGridLine(segmentCellXTo, 0, segmentCellXTo, this.props.height);
+            this.drawGridLine(segmentCellXFrom, 0, segmentCellXFrom, this.height);
+            this.drawGridLine(segmentCellXTo, 0, segmentCellXTo, this.height);
         }
     }
 
-    drawHorizontalGridLines(){
-        for(let i = 0; this.props.segmentsInStack > i; i++){
-            let segmentCellYFrom = this.props.height - ((this.segmentHeight + this.padding) * i) + 0.5;
+    drawHorizontalGridLines() {
+        for (let i = 0; this.props.segmentsInStack > i; i++) {
+            let segmentCellYFrom = this.height - ((this.segmentFullHeight * i) + this.padding) - 0.5;
             let segmentCellYTo = segmentCellYFrom + this.segmentHeight;
 
-            this.drawGridLine(0, segmentCellYFrom, this.props.width, segmentCellYFrom);
-            this.drawGridLine(0, segmentCellYTo, this.props.width, segmentCellYTo);
+            // this.drawGridLine(0, segmentCellYFrom, this.width, segmentCellYFrom);
+            this.drawGridLine(0, segmentCellYTo, this.width, segmentCellYTo);
         }
     }
 
-    drawGridLine(fromX, fromY, toX, toY){
-        this.analyserContext.strokeStyle = this.props.gridColor;
-        this.analyserContext.beginPath();
-        this.analyserContext.moveTo(fromX, fromY);
-        this.analyserContext.lineTo(toX, toY);
-        this.analyserContext.stroke();
+    drawGridLine(fromX, fromY, toX, toY) {
+        this.gridContext.strokeStyle = this.props.gridColor;
+        this.gridContext.beginPath();
+        this.gridContext.moveTo(fromX, fromY);
+        this.gridContext.lineTo(toX, toY);
+        this.gridContext.stroke();
     }
 
-    loop(){
+    loop() {
         requestAnimationFrame(this.loop.bind(this));
 
-        if(this.props.analyser){
+        if (this.props.analyser) {
             var analyserValues = this.props.analyser.analyse();
             this.draw(analyserValues);
         }
@@ -113,9 +120,7 @@ class Analyser extends Component {
 Analyser.propTypes = {
     analyser: PropTypes.object,
     segmentsInStack: PropTypes.number,
-    width: PropTypes.number,
-    height: PropTypes.number,
     gridColor: PropTypes.string,
 };
 
-export default CSSModules(Analyser, styles, {allowMultiple: true});
+export default CSSModules(Analyser, styles, { allowMultiple: true });
