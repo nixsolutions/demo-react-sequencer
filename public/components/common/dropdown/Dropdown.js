e10af7e1126bcdcbe195853f6551aaad38360b38
jsruvi <jsruvi@gmail.com>
2016-12-20 18:30:22 +0200

    dropdown styles

--- public/components/common/dropdown/Dropdown.js
+++ public/components/common/dropdown/Dropdown.js
@@ -1,44 +1,53 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
-import React, {Component, PropTypes} from 'react';
-import Dropdown, {DropdownTrigger, DropdownContent} from 'react-simple-dropdown';
+import React, { Component, PropTypes } from 'react';
+import Dropdown, { DropdownTrigger, DropdownContent } from 'react-simple-dropdown';
 import ScrollableBlock from 'components/common/scrollableBlock/ScrollableBlock';
 
 class DropdownComponent extends Component {
-    constructor (props, state) {
-        super(props, state);
+    static defaultProps = {
+        styleMode: 'style1'
+    };
 
-        this.state = {active: false};
-    }
+    state = { active: false };
 
     render() {
         let items = this.props.items.map((item, i) => {
             return <li key={i} onClick={this.onSelect.bind(this, item.value)}>{item.title}</li>
         });
         let content = items.length ? (<ul styleName="dropdown-list">{items}</ul>) : this.props.children;
-        let cssClass = ['dropdown', this.state.active ? 'active' : ''].join(' ');
+        let activeClass = this.state.active ? 'active' : '';
+        let cssClass = ['dropdown', this.props.styleMode, activeClass].join(' ');
+        let dropdownProps = {
+            styleName: cssClass,
+            onShow: this.onShow.bind(this),
+            onHide: this.onHide.bind(this),
+            ref: 'dropdown'
+        }
 
-        return <Dropdown styleName={cssClass} 
-                        onShow={this.onShow.bind(this)} 
-                        onHide={this.onHide.bind(this)} ref="dropdown">
-                    <DropdownTrigger styleName="dropdown-trigger-area">{this.props.title}</DropdownTrigger>
+        return <div styleName={`dropdown-holder ${this.props.styleMode}`}>
+            <div styleName="dropdown-wrapper">
+                <Dropdown {...dropdownProps}>
+                    <DropdownTrigger styleName="dropdown-trigger-area">{this.props.title} <span styleName="arrow"></span></DropdownTrigger>
                     <DropdownContent>
                         <div styleName="holder">
                             <ScrollableBlock autoHeightMax={122}>{content}</ScrollableBlock>
                         </div>
                     </DropdownContent>
                 </Dropdown>
+            </div>
+        </div>
     }
 
-    onShow(){
-        this.setState({active: true});
+    onShow() {
+        this.setState({ active: true });
     }
 
-    onHide(){
-        this.setState({active: false});
+    onHide() {
+        this.setState({ active: false });
     }
 
-    onSelect(value, e){
+    onSelect(value, e) {
         this.refs.dropdown.hide();
         this.props.onSelect(value);
     }
@@ -54,4 +63,4 @@ DropdownComponent.propTypes = {
     onSelect: PropTypes.func.isRequired
 };
 
-export default CSSModules(DropdownComponent, styles, {allowMultiple: true});
+export default CSSModules(DropdownComponent, styles, { allowMultiple: true });
