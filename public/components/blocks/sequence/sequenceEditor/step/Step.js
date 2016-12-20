fb815c60b8dc6c7340fdbfcb4ee0237072987e25
jsruvi <jsruvi@gmail.com>
2016-12-20 16:20:03 +0200

    steps styles

--- public/components/blocks/sequence/sequenceEditor/step/Step.js
+++ public/components/blocks/sequence/sequenceEditor/step/Step.js
@@ -4,11 +4,10 @@ import React, {Component, PropTypes} from 'react';
 
 class Step extends Component {
     render() {
-        let {active, isEven, isHighlighted} = this.props;
+        let {active, isEven} = this.props;
         let evenClass = isEven ? 'even' : '';
-        let highlightClass = isHighlighted ? 'highlight' : '';
         let activeClass = active ? 'active' : '';
-        let className = ['step', evenClass, activeClass, highlightClass].join(' ');
+        let className = ['step', evenClass, activeClass].join(' ');
 
         return <div styleName={className}></div>
     }
@@ -21,7 +20,6 @@ class Step extends Component {
 Step.propTypes = {
     active: PropTypes.bool,
     isEven: PropTypes.bool,
-    isHighlighted: PropTypes.bool,
 };
 
 export default CSSModules(Step, styles, {allowMultiple: true});
