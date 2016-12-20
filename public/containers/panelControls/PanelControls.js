0c154f46a97d77bc0f65795f87701296e5439352
jsruvi <jsruvi@gmail.com>
2016-12-20 12:20:58 +0200

    refactoring

--- public/containers/panelControls/PanelControls.js
+++ public/containers/panelControls/PanelControls.js
@@ -1,22 +1,26 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import VolumeController from 'components/blocks/volumeController/VolumeController';
 import Analyser from 'components/blocks/analyser/Analyser';
-import {updateVolume} from 'modules/volume';
+import { updateVolume } from 'modules/volume';
 
 class PanelControls extends Component {
     render() {
+        let volumeControllerProps = {
+            value: this.props.volume,
+            onChange: this.props.updateVolume
+        }
+
         return (
-                <div styleName="panel-controls">
-                    <VolumeController value={this.props.volume} 
-                                    onChange={this.props.updateVolume}/>
-                    <Analyser analyser={this.props.analyser}/>
-                 </div>
+            <div styleName="panel-controls">
+                <VolumeController {...volumeControllerProps}/>
+                <Analyser analyser={this.props.analyser} />
+            </div>
         )
     }
-    
+
 }
 
 PanelControls.propTypes = {
@@ -28,7 +32,7 @@ export default connect(mapStateToProps, {
     updateVolume,
 })(CSSModules(PanelControls, styles));
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
         volume: state.volume,
         analyser: state.analyser
