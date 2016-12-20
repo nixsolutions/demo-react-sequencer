10de102093a5f102aa3897a84bd0fb62c26e4430
jsruvi <jsruvi@gmail.com>
2016-12-20 12:27:37 +0200

    props refactoring

--- public/components/blocks/playControls/PlayControls.js
+++ public/components/blocks/playControls/PlayControls.js
@@ -1,37 +1,43 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import PlayButton from 'components/common/buttons/playButton/PlayButton';
 import PauseButton from 'components/common/buttons/pauseButton/PauseButton';
 import StopButton from 'components/common/buttons/stopButton/StopButton';
-import {SPACE_KEY} from 'utils/keys';
+import { SPACE_KEY } from 'utils/keys';
 
 class PlayControls extends Component {
-    componentWillMount(){
+    componentWillMount() {
         this.bindPlay();
     }
 
     render() {
+        let playButtonProps = {
+            active: (this.props.playState === 'play'),
+            onClick: this.props.updatePlay
+        }
+
+        let pauseButtonProps = {
+            active: (this.props.playState === 'pause'),
+            onClick: this.props.updatePlay
+        }
+
+        let stopButtonProps = {
+            active: (this.props.playState === 'stop'),
+            disabled: (this.props.playState === 'stop'),
+            onClick: this.props.updatePlay
+        }
+
         return (
             <ul styleName="play-controls">
-                <li>
-                    <PlayButton active={this.props.playState === 'play'}
-                            onClick={this.props.updatePlay}></PlayButton>
-                </li>
-                <li>
-                    <PauseButton active={this.props.playState === 'pause'}
-                            onClick={this.props.updatePlay}></PauseButton>
-                </li>
-                <li>
-                    <StopButton active={this.props.playState === 'stop'}
-                            disabled={this.props.playState === 'stop'}
-                            onClick={this.props.updatePlay}></StopButton>
-                </li>
+                <li><PlayButton {...playButtonProps} /></li>
+                <li><PauseButton {...pauseButtonProps} /></li>
+                <li><StopButton {...stopButtonProps} /></li>
             </ul>
         )
     }
 
-    bindPlay(){
+    bindPlay() {
         let onMouseDown = (e) => {
             let value = (this.props.playState === 'play') ? 'pause' : 'play';
 
@@ -51,7 +57,7 @@ PlayControls.propTypes = {
     bindToKey: PropTypes.func,
 }
 
-export default CSSModules(PlayControls, styles, {allowMultiple: true});
+export default CSSModules(PlayControls, styles, { allowMultiple: true });
 
 
 
