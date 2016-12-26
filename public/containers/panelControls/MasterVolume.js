0c9a38ba23e7ef64d1832d981a498467da82f7f5
jsruvi <jsruvi@gmail.com>
2016-12-26 11:14:00 +0200

    smart component for MasterVolume

---
+++ public/containers/panelControls/MasterVolume.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import VolumeController from 'components/blocks/volumeController/VolumeController';
+import {connect} from 'react-redux';
+import { updateVolume } from 'modules/volume';
+
+export default connect(mapStateToProps, {
+   onChange: updateVolume 
+})(VolumeController);
+
+function mapStateToProps(state) {
+    return {
+        value: state.volume
+    };
+}
--- public/containers/panelControls/PanelControls.js
+++ public/containers/panelControls/PanelControls.js
@@ -1,38 +1,19 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import VolumeController from 'components/blocks/volumeController/VolumeController';
+
+import MasterVolume from './MasterVolume';
 import Analyser from './Analyser';
-import { updateVolume } from 'modules/volume';
 
 class PanelControls extends Component {
     render() {
-        let volumeControllerProps = {
-            value: this.props.volume,
-            onChange: this.props.updateVolume
-        }
-
         return (
             <div styleName="panel-controls">
-                <VolumeController {...volumeControllerProps}/>
+                <MasterVolume />
                 <Analyser />
             </div>
-        )
+        );
     }
-
 }
 
-PanelControls.propTypes = {
-    updateVolume: PropTypes.func,
-};
-
-export default connect(mapStateToProps, {
-    updateVolume,
-})(CSSModules(PanelControls, styles));
-
-function mapStateToProps(state) {
-    return {
-        volume: state.volume,
-    };
-}
+export default CSSModules(PanelControls, styles);
