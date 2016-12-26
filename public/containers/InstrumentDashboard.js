9403af3a621b675937e0cb9fec43a756a0317dc5
jsruvi <jsruvi@gmail.com>
2016-12-26 18:11:18 +0200

    InstrumentDashboard container

---
+++ public/containers/InstrumentDashboard.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import InstrumentDashboard from 'components/blocks/sequence/instrumentDashboard/InstrumentDashboard';
+import {connect} from 'react-redux';
+import {updateBPM} from 'modules/bpm';
+
+export default connect(mapStateToProps)(InstrumentDashboard);
+
+function mapStateToProps(state, props) {
+    return {
+        instrumentName: state.instruments.byId[props.instrumentId].name
+    };
+}
