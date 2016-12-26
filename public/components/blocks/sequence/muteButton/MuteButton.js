d7e87f25ec0f18b290455c246b010f53e85f91a6
jsruvi <jsruvi@gmail.com>
2016-12-26 17:13:57 +0200

    mute button component

---
+++ public/components/blocks/sequence/muteButton/MuteButton.js
@@ -0,0 +1,36 @@
+import CSSModules from 'react-css-modules';
+import styles from './styles.less';
+import React, { Component, PropTypes } from 'react';
+
+class MuteButton extends Component {
+    static propTypes = {
+        instrumentId: PropTypes.string,
+        toggleInstrument: PropTypes.func,
+    };
+
+    constructor(props){
+        super(props);
+
+        this.toggleInstrument = this.toggleInstrument.bind(this);
+    }
+
+    render() {
+        let {isActive} = this.props;
+        let muteClass = ['mute', isActive ? 'active' : ''].join(' ');
+        let muteText = isActive ? 'disable instrument' : 'enable instrument';
+    
+        let muteProps = {
+            styleName: muteClass,
+            title: muteText,
+            onClick: this.toggleInstrument
+        };
+
+        return <div {...muteProps}></div>;
+    }
+
+    toggleInstrument(){
+        this.props.toggleInstrument(this.props.instrumentId);
+    }
+}
+
+export default CSSModules(MuteButton, styles, { allowMultiple: true });
