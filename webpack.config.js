93de0d606e9085a3e66855ccde62af69d9b21662
jsruvi <jsruvi@gmail.com>
2016-12-27 17:50:30 +0200

    updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,19 @@ var webpack = require('webpack');
 var SpritesmithPlugin = require('webpack-spritesmith');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './public/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new SpritesmithPlugin({
+    devtool: 'source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './public/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new SpritesmithPlugin({
             src: {
                 cwd: path.resolve(__dirname, 'public/images'),
                 glob: '*.png'
@@ -29,36 +28,36 @@ module.exports = {
                 cssImageRef: "/shared/sprite.png"
             }
         })
-  ],
-  module: {
-    loaders: [
-        {
-            test: /\.js$/,
-            loaders: ['react-hot', 'babel'],
-            include: path.join(__dirname, 'public'),
-            plugins: ['transform-runtime']
-        },
-        {
-            test: /\.less$/,
-            loaders: [
-                'style?sourceMap',
-                'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-                'less'
-            ]
-        },
-        {
-            test: /\.(png|jpg|svg|woff|woff2|eot|ttf|gif)$/,
-            exclude: /node_modules/,
-            loader: "url?name=[path][name].[ext]&limit=4096"
-        },
-        {
-            test: /\.(png|jpg|svg|woff|woff2|eot|ttf|gif)$/,
-            exclude: /node_modules/,
-            loader: "file"
-        }
-    ]
-  },
-  resolve: {
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.join(__dirname, 'public'),
+                plugins: ['transform-runtime']
+            },
+            {
+                test: /\.less$/,
+                loaders: [
+                    'style?sourceMap',
+                    'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+                    'less'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|woff|woff2|eot|ttf|gif)$/,
+                exclude: /node_modules/,
+                loader: "url?name=[path][name].[ext]&limit=4096"
+            },
+            {
+                test: /\.(png|jpg|svg|woff|woff2|eot|ttf|gif)$/,
+                exclude: /node_modules/,
+                loader: "file"
+            }
+        ]
+    },
+    resolve: {
         modulesDirectories: ['node_modules'],
         alias: {
             components: path.join(__dirname, '/public/components'),
@@ -72,3 +71,15 @@ module.exports = {
         extensions: ['', '.js', '.less']
     }
 };
+
+if (process.env.NODE_ENV === 'production'){
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    )
+}
