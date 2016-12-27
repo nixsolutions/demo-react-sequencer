4ec00305c8362896c5f3f375ac2a32c1c1d600a0
jsruvi <jsruvi@gmail.com>
2016-12-27 11:35:03 +0200

    reworked SequenceEditor component

--- public/components/blocks/sequence/sequenceEditor/SequenceEditor.js
+++ public/components/blocks/sequence/sequenceEditor/SequenceEditor.js
@@ -1,58 +1,36 @@
 import CSSModules from 'react-css-modules';
 import styles from './styles.less';
 import React, {Component, PropTypes} from 'react';
-import Step from './step/Step';
+import SequenceStep from 'containers/SequenceStep';
 
 class SequenceEditor extends Component {
     static propTypes = {
-        playedStep: PropTypes.number,
-        instruments: PropTypes.arrayOf(PropTypes.shape({
-                name: PropTypes.string,
-                volume: PropTypes.number,
-                path: PropTypes.string,
-                active: PropTypes.bool,
-                notes: PropTypes.array
-            })
-        ),
-        onToggleStep: PropTypes.func
+        instrumentId: PropTypes.string,
+        steps: PropTypes.array,
     };
 
-    constructor(props){
-        super(props);
-
-        this.onStepClick = this.onStepClick.bind(this);
-    }
-
     render() {
-        let {instrument} = this.props; 
-        let items = this.createSteps(instrument.notes);
+        let {steps} = this.props; 
+        let items = this.createSteps(steps);
+
         return <ul styleName="sequence-editor">{items}</ul>
     }
 
-    createSteps(notes){
-        return notes.map((note, i) => {
-            let isEven = Math.floor(i / 4) % 2 !== 0;
-            let isHighlighted = this.props.playedStep === i;
+    createSteps(steps){
+        let {instrumentId} = this.props;
+
+        return steps.map((step, i) => {
             let stepProps = {
-                active: (note !== undefined),
-                isHighlighted,
-                isEven,
-                index: i,
-                note,
-                onStepClick: this.onStepClick
+                instrumentId,
+                indexInSequence: i
             };
 
             return (
                 <li key={i}>
-                    <Step {...stepProps}/>
+                    <SequenceStep {...stepProps} />
                 </li>
             );
         })
-    }
-
-    onStepClick(note, stepIndex){
-        this.props.onToggleStep &&
-        this.props.onToggleStep(note, stepIndex, this.props.instrument);
     }
 }
 
