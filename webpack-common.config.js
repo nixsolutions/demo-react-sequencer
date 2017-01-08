721af2d587a995962211df198770cf962629fa35
jsruvi <jsruvi@gmail.com>
2017-01-08 14:27:34 +0200

    setting NODE_ENV mode in webpack.config

--- webpack-common.config.js
+++ webpack-common.config.js
@@ -15,6 +15,9 @@ module.exports = {
         publicPath: '/docs/'
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
         new SpritesmithPlugin({
             src: {
                 cwd: path.resolve(__dirname, 'public/images'),
--- webpack-githubpages.config.js
+++ webpack-githubpages.config.js
@@ -12,6 +12,9 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false,
