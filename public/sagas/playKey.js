debdc017d92b95ddafa64a6002820fd72bf4d850
jsruvi <jsruvi@gmail.com>
2017-01-08 23:44:22 +0200

    playKey saga

--- public/sagas/playKey.js
+++ public/sagas/playKey.js
@@ -1,30 +1,61 @@
-import { take, call, put, fork, cancel } from 'redux-saga/effects';
-// import { LOGIN_START, LOGOUT, LOGIN_SUCCESS, LOGIN_FAIL } from 'modules/login';
+import { take, call, put, fork, cancel, select, race } from 'redux-saga/effects';
+import { ADD_PLAYED_NOTE } from 'modules/playedNotes';
+import { UPDATE_ACCOMPANIMENT_INSTRUMENT, updateAccompanimentInstrument } from 'modules/accompanimentInstrument';
+import { noteToPitch, percentsToDecibels} from 'utils/notes';
+import { pianoVolume, samples } from 'selectors';
+import Tone from 'tone';
 
+const buffer = new Tone.Buffer();
 
-// function* authorizeUser(user, password) {
-//     try {
-//         const data = yield call(authorize, user, password);
-//         yield put({ type: LOGIN_SUCCESS, payload: data });
-//         yield call(storeToken, data);
-//     } catch (e) {
-//         yield put({ type: LOGIN_FAIL, payload: e });
-//     }
-// }
+const playNote = (sample, note, volume) => {
+    const pitch = noteToPitch(note);
 
-export default function* playKey() {
-    // while (true) {
-    //     const { payload } = yield take(LOGIN_START);
-    //     const { name, password } = payload;
-    //
-    //     const authorizeTask = yield fork(authorizeUser, name, password);
-    //
-    //     const { type } = yield take([LOGOUT, LOGIN_FAIL]);
-    //
-    //     yield call(clearToken);
-    //
-    //     if (type === LOGOUT) {
-    //         yield cancel(authorizeTask);
-    //     }
-    // }
+    sample.volume.value = percentsToDecibels(volume);
+    sample.triggerAttackRelease(pitch);
+};
+
+function* initSample(){
+    const samplesSet = yield select(samples);
+    yield put(updateAccompanimentInstrument(samplesSet[0]))
 }
+
+function* loadSample(path){
+    return yield new Promise((resolve, reject) => {
+        buffer.load(path, (buffer) => {
+            const sample = new Tone.Sampler(buffer).toMaster();
+            resolve(sample);
+        }, reject);
+    });
+}
+
+function* sampleManager(){
+    const { payload } = yield take(UPDATE_ACCOMPANIMENT_INSTRUMENT);
+
+    return yield loadSample(payload.path);
+}
+
+function* playKey(sample) {
+    while (true) {
+        const { payload } = yield take(ADD_PLAYED_NOTE);
+        const volume = yield select(pianoVolume);
+
+        yield fork(playNote, sample, payload, volume);
+    }
+}
+
+export default function* playKeyWatch(){
+    yield fork(initSample);
+
+    let playKeyTask;
+
+    while(true){
+        const sample = yield call(sampleManager);
+
+        if(playKeyTask){
+            yield cancel(playKeyTask);
+        }
+
+        playKeyTask = yield fork(playKey, sample);
+    }
+}
+
