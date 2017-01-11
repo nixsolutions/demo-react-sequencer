b6994e5bd1225981b101d29ba0bcd31388cec499
jsruvi <jsruvi@gmail.com>
2017-01-11 17:21:02 +0200

    updated selectors

--- public/selectors/index.js
+++ public/selectors/index.js
@@ -4,6 +4,9 @@ export const pianoVolume = state => state.pianoVolume;
 export const samples = state => state.samples;
 export const instrumentsSelector = state => state.instruments.byId;
 export const instrumentsStepsSelector = state => state.instrumentsSteps;
+export const stepsAmountSelector = state => state.stepsAmount;
+export const initialSamplesSelector = state => state.samples.slice(0, 3);
+export const instrumentsEffectsSelector = state => state.instrumentsEffects;
 export const samplesPathsSelector = state => state.samples.map(sample => sample.path);
 
 export const makeDropdownItems = () => createSelector(
